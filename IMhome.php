6fd101b3c31f9a14dbfeeae1ab4341eef055f862
Travis Roser <travisroser@email.arizona.edu>
2017-02-15 13:35:45 -0700

    Created IM Portal
    2/15/17

---
+++ IMhome.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link type='text/css' href='styles.css' rel='stylesheet'>
+  <link rel='icon' href='./icon.ico'>
+  <title>Vijilis Portal</title>
+</head>
+<body>
+  <div class="headerbar">
+		<a href="./index.php"><img src="logo_rn.png" height="80%" style="padding-top: 8px; padding-left: 5px;"></a>
+    <nav>
+      <a href="./IMhome.php">Home</a>
+      <a href="./IMrequests.php">Requests</a>
+      <a href="./IMaccount.php">Account</a>
+      <a href="./IMvendors.php">Vendors</a>
+    </nav>
+	</div>
+  <br/><br/><br/><br/><br/>
+  <div class="sb">
+    <div class="sbh">
+      Header Bar
+    </div>
+    <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+  </div>
+</body>
+</html>
